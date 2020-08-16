3c0583941952104abf226c1f349e9d2ec8d843df
Jaden Locke <jadenlocke1113@gmail.com>
2020-08-16 16:31:03 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -5,10 +5,12 @@ def update_status
          :matriarch => {name: "Lady Montague", age: "54"},
          :hero => {name: "Romeo", age: "15", status: "alive"},
          :hero_friends => [
-            {name: "Benvolio", age: "17", attitude: "worried"},
+            {name: "Benvolio", age: "17", attitude: "worried"}
             {name: "Mercutio", age: "18", attitude: "hot-headed"}
-         ]
+    [
       },
+    end 
+
       :capulet => {
          :patriarch => {name: "Lord Capulet", age: "50"},
          :matriarch => {name: "Lady Capulet", age: "51"},
@@ -18,17 +20,18 @@ def update_status
          {name: "Nurse", age: "44", attitude: "worried"}
          ]
       }
-   }
-
-  # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
-  # Code your solution here:
-
+  ],
+end 
+  
 
 
 
   # After your code, the following code will return the altered hash. This method must only return the modified epic_tragedy hash
-  epic_tragedy
-end
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
+  
+
+ puts epic_tragedy
 
 def add_characters
    epic_tragedy = {
@@ -51,6 +54,7 @@ def add_characters
          ]
       }
    }
+ 
 
    # Write code below that does the following programmatically:
    #
@@ -59,6 +63,11 @@ def add_characters
    #   - The first hash should have the :name key set to "Prince Escalus" 
    #   - The second hash should have the :name key set to "Apothecary" 
    # Code your solution here:
+   epic_tragedy [:additional_characters] = [ 
+     { name: "Prince Escalus" }, 
+     { name: "Apothecary" }
+     
+     ]
 
 
 
@@ -67,4 +76,4 @@ def add_characters
 
    # After your code, the following line will return the altered hash
    epic_tragedy
-end
+ end 
