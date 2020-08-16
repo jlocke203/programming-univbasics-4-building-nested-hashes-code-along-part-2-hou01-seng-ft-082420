cfcf01614ff96370fe6c869c3608c3b30acf252f
Jaden Locke <jadenlocke1113@gmail.com>
2020-08-16 16:35:55 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -9,7 +9,7 @@ def update_status
             {name: "Mercutio", age: "18", attitude: "hot-headed"}
     [
       },
-      epic_tragedy[:montague][:hero]
+      epic_tragedy[:montague][:hero][:status]
     end 
 
       :capulet => {
