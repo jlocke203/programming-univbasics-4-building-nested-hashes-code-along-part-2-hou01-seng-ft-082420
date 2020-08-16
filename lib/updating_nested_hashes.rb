007a8d1f2d2b273025077f2ea62872ed3a36c279
Jaden Locke <jadenlocke1113@gmail.com>
2020-08-16 16:58:03 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -8,7 +8,7 @@ def update_status
             {name: "Benvolio", age: "17", attitude: "worried"}
             {name: "Mercutio", age: "18", attitude: "hot-headed"}
     [
-      },
+      {,
 ]
 
       :capulet => {
